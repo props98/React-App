a2882067bb2e5dc5d9c3f5f276b37b7e3b3ed2c9
Max <mp@Mbp.local>
2023-07-09 18:21:36 +0300

    Add basic React structre for my app project / link bootstrap & fontAwsome

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Mr. Anderson</h1>
+      <h2>5 posts, likes 0</h2>
+    </div>
+  )
+}
+
+export default AppHeader;
