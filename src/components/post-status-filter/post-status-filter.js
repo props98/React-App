feab95f1bbc49f43e1a7935356a946e3e4008118
Max <mp@Mbp.local>
2023-07-09 18:49:36 +0300

    Link styles files to comp. DHM

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import './post-status-filter.css';
 
 const PostStatusFilter = () => {
   return (
     <div className="btn-group">
-      <button/>
-      <button/>
+      <button className="btn btn-primary">All</button>
+      <button className="btn btn-outline-secondary">Likes</button>
     </div>
   )
 }
